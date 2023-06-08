2cd492b086751ce319f94ac375ec64f9a88b2c5f
SerGGe43 <seregaglad007@gmail.com>
2023-06-08 16:42:41 +0300

    ZAEBALO

---
+++ Assets/Cell.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public GameObject WallLeft;
+    public GameObject WallBottom;
+
+    public void Start()
+    {
+        Destroy(this.gameObject, 0.1f);
+    }
+}
---
+++ Assets/MazeGenerator.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Threading;
+using UnityEngine;
+
+public class MazeGenerator : MonoBehaviour
+{
+
+    public Cell CellPrefab;
+    public Vector3 CellSize = new Vector3(1, 1, 0);
+    public HintRenderer HintRenderer;
+    public int Width = 10;
+    public int Height = 10;
+    public bool isRemoved = false;
+    public Maze maze;
+    private LineRenderer componentLineRenderer;
+
+    private void Start()
+    {
+       // MazeGenerator generator = new MazeGenerator();
+        maze = GenerateMaze();
+        componentLineRenderer = GetComponent<LineRenderer>();
+
+        /*for (int x = 0; x < maze.cells.GetLength(0); x++)
+        {
+            for (int y = 0; y < maze.cells.GetLength(1); y++)
+            {
+                Cell c = Instantiate(CellPrefab, new Vector3(x * CellSize.x, y * CellSize.y, y * CellSize.z), Quaternion.identity);
+
+                c.WallLeft.SetActive(maze.cells[x, y].WallLeft);
+                c.WallBottom.SetActive(maze.cells[x, y].WallBottom);
+            }
+        }*/
+
+        //HintRenderer.DrawPath();
+    }
+
+    private void Update()
+    {
+        for (int x = 0; x < maze.cells.GetLength(0); x++)
+        {
+            for (int y = 0; y < maze.cells.GetLength(1); y++)
+            {
+                Cell c = Instantiate(CellPrefab, new Vector3(x * CellSize.x, y * CellSize.y, y * CellSize.z), Quaternion.identity);
+
+                c.WallLeft.SetActive(maze.cells[x, y].WallLeft);
+                c.WallBottom.SetActive(maze.cells[x, y].WallBottom);
+            }
+        }
+        if (isRemoved)
+        {
+            maze.finishPosition = PlaceMazeExit(maze.cells);
+            isRemoved = false;
+        }
+
+    }
+
+    public Maze GenerateMaze()
+    {
+        MazeGeneratorCell[,] cells = new MazeGeneratorCell[Width, Height];
+
+        for (int x = 0; x < cells.GetLength(0); x++)
+        {
+            for (int y = 0; y < cells.GetLength(1); y++)
+            {
+                cells[x, y] = new MazeGeneratorCell {X = x, Y = y};
+            }
+        }
+
+        for (int x = 0; x < cells.GetLength(0); x++)
+        {
+            cells[x, Height - 1].WallLeft = false;
+        }
+
+        for (int y = 0; y < cells.GetLength(1); y++)
+        {
+            cells[Width - 1, y].WallBottom = false;
+        }
+
+        StartCoroutine(RemoveWallsWithBacktracker(cells));
+        //while (!isRemoved) ;
+
+
+        Maze maze = new Maze();
+
+        maze.cells = cells;
+        //maze.finishPosition = PlaceMazeExit(cells);
+
+        return maze;
+    }
+
+    IEnumerator RemoveWallsWithBacktracker(MazeGeneratorCell[,] maze)
+    {
+        bool tp = true;
+        float timeTP;
+        MazeGeneratorCell current = maze[0, 0];
+        current.Visited = true;
+        current.DistanceFromStart = 0;
+
+        Stack<MazeGeneratorCell> stack = new Stack<MazeGeneratorCell>();
+        do
+        {
+            List<MazeGeneratorCell> unvisitedNeighbours = new List<MazeGeneratorCell>();
+
+            int x = current.X;
+            int y = current.Y;
+
+            if (x > 0 && !maze[x - 1, y].Visited) unvisitedNeighbours.Add(maze[x - 1, y]);
+            if (y > 0 && !maze[x, y - 1].Visited) unvisitedNeighbours.Add(maze[x, y - 1]);
+            if (x < Width - 2 && !maze[x + 1, y].Visited) unvisitedNeighbours.Add(maze[x + 1, y]);
+            if (y < Height - 2 && !maze[x, y + 1].Visited) unvisitedNeighbours.Add(maze[x, y + 1]);
+
+            if (unvisitedNeighbours.Count > 0)
+            {
+                MazeGeneratorCell chosen = unvisitedNeighbours[UnityEngine.Random.Range(0, unvisitedNeighbours.Count)];
+                //yield return new WaitForSeconds(0.5f);
+                //Invoke("pause", 20f);
+                RemoveWall(current, chosen);
+                chosen.Visited = true;
+                stack.Push(chosen);
+                chosen.DistanceFromStart = current.DistanceFromStart + 1;
+                current = chosen;
+                Debug.Log("Before");
+                yield return new WaitForSeconds(0.1f);
+                Debug.Log("after");
+            }
+            else
+            {
+                current = stack.Pop();
+            }
+            //yield return new WaitForSeconds(0.5f);
+        } while (stack.Count > 0);
+        isRemoved = true;
+        HintRenderer.DrawPath();
+        //isRemoved = true;
+        //yield return null;
+
+    }
+
+    private void RemoveWall(MazeGeneratorCell a, MazeGeneratorCell b)
+    {
+        if (a.X == b.X)
+        {
+            if (a.Y > b.Y) a.WallBottom = false;
+            else b.WallBottom = false;
+        }
+        else
+        {
+            if (a.X > b.X) a.WallLeft = false;
+            else b.WallLeft = false;
+        }
+    }
+
+    private Vector2Int PlaceMazeExit(MazeGeneratorCell[,] maze)
+    {
+        MazeGeneratorCell furthest = maze[0, 0];
+
+        for (int x = 0; x < maze.GetLength(0); x++)
+        {
+            if (maze[x, Height - 2].DistanceFromStart > furthest.DistanceFromStart) furthest = maze[x, Height - 2];
+            if (maze[x, 0].DistanceFromStart > furthest.DistanceFromStart) furthest = maze[x, 0];
+        }
+
+        for (int y = 0; y < maze.GetLength(1); y++)
+        {
+            if (maze[Width - 2, y].DistanceFromStart > furthest.DistanceFromStart) furthest = maze[Width - 2, y];
+            if (maze[0, y].DistanceFromStart > furthest.DistanceFromStart) furthest = maze[0, y];
+        }
+
+        if (furthest.X == 0) furthest.WallLeft = false;
+        else if (furthest.Y == 0) furthest.WallBottom = false;
+        else if (furthest.X == Width - 2) maze[furthest.X + 1, furthest.Y].WallLeft = false;
+        else if (furthest.Y == Height - 2) maze[furthest.X, furthest.Y + 1].WallBottom = false;
+
+        return new Vector2Int(furthest.X, furthest.Y);
+    }
+
+    void pause ()
+    {
+
+    }
+
+
+}
+
